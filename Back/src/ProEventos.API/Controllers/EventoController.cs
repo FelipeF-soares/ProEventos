072b2d7bc8b4aebe48668294d6752f33653c48f6
FelipeF-soares <felipe.fends@gmail.com>
2024-02-27 15:16:50 -0300

    Iniciando Projeto

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Model;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly ILogger<EventoController> _logger;
+
+        public IEnumerable<Evento> _eventos = new Evento[]{
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular e .NET 5",
+                    Local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                    ImageURL = "foto.png"
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "Angular e Suas Novidades",
+                    Local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(10).ToString("dd/MM/yyyy"),
+                    ImageURL = "foto1.png"
+                }
+            };
+
+        public EventoController()
+        {  
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _eventos;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id)
+        {
+            return _eventos.Where(evento => evento.EventoId.Equals(id));
+        }
+    }
+}
